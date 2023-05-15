205d0ed3f26a33c00e9c82fef054ede991ce1a3e
fatma taha <94760329+fatmaT2001@users.noreply.github.com>
2023-05-15 18:01:58 +0300

    connecting the main page to the data base

---
+++ ZewailCiryScienceWeek/Models/FestivalCommitteeClass.cs
@@ -0,0 +1,16 @@
+﻿namespace ZewailCiryScienceWeek.DataClasses
+{
+    public class FestivalCommitteeClass
+    {
+        public int CommitteeId { get; set; }
+        public string CommitteeName { get; set; }
+        public string CommitteeDescription { get; set; }
+
+        public FestivalCommitteeClass(int committeeId = 0, string committeeName = "", string committeeDescription = "")
+        {
+            CommitteeId = committeeId;
+            CommitteeName = committeeName;
+            CommitteeDescription = committeeDescription;
+        }
+    }
+}
--- ZewailCiryScienceWeek/Program.cs
+++ ZewailCiryScienceWeek/Program.cs
@@ -1,8 +1,10 @@
+using ZewailCiryScienceWeek.DataClasses;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddSingleton<DataBase>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
