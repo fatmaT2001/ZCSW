3e6b078890f7ec7149d5e120eee20633f6af800c
Shrouk Elkot <118758698+ShroukElkot@users.noreply.github.com>
2023-05-18 23:11:52 +0200

    lastChanges

--- ZewailCiryScienceWeek/Pages/Admin/promocode.cshtml.cs
+++ ZewailCiryScienceWeek/Pages/Admin/promocode.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.Data;
 namespace projecttt.Pages
 {
     public class promocodeModel : PageModel
     {
+        public DataTable dt { get; set; }
         public void OnGet()
         {
+            // dt = ReadTable("");
         }
     }
 }
--- ZewailCiryScienceWeek/Pages/Admin/tickets.cshtml.cs
+++ ZewailCiryScienceWeek/Pages/Admin/tickets.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.Data;
 namespace projecttt.Pages
 {
     public class ticketsModel : PageModel
     {
+        public DataTable dt { get; set; }
         public void OnGet()
         {
+            // dt = ReadTable("");
         }
     }
 }
