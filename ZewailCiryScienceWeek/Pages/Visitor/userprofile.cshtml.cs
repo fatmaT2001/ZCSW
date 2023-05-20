969d1042d9f933f981583edbe1df8cfa666decad
sherinmohamed0 <117950264+sherinmohamed0@users.noreply.github.com>
2023-05-20 17:22:47 +0300

    EDIT

--- ZewailCiryScienceWeek/Pages/Visitor/userprofile.cshtml.cs
+++ ZewailCiryScienceWeek/Pages/Visitor/userprofile.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZewailCiryScienceWeek.DataClasses;
+using ZewailCiryScienceWeek.Models;
 
 namespace ZewailCiryScienceWeek.Pages.Visitor
 {
     public class userprofileModel : PageModel
     {
+
+        private readonly DataBase db;
+
+        [BindProperty]
+        public Person p1 { get; set; }
+
+        [BindProperty]
+        public visitor v1 { get; set; }
+        [BindProperty]
+        public string msg { get; set; }
         public void OnGet()
         {
         }
+        
     }
 }
